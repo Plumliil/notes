eddda726480ab5d35f143331b80d6526190ebf99
Plumliil <476453574@qq.com>
2021-08-30 18:34:12 +0800

    Express 中间件

--- Express/Code/05/app.js
+++ Express/Code/05/app.js
@@ -2,34 +2,76 @@ const express=require('express');
 
 const app=express();
 
-app.get('/',(req,res)=>{
-    console.log(req.url);// 请求地址
-    console.log(req.method); // 请求方法
-    console.log(req.headers); // 请求头
-    console.log('请求参数',req.query); // 传入参数
 
-    // 设置响应状态码
-    res.statusCode=200;
+// req.body获取请求体
+// app.use(express.json());
+// app.use(express.urlencoded());
+//
+// const mylogger=(req)=>{
+//     console.log(req.method,req.url,Date.now())
+// }
 
-    // 结束响应
-    // res.end();
 
-    // res.write('a');
-    // res.write('b');
-    // res.write('c');
-    //
-    // res.end('Hello World')
 
-    // res.send('Hello Word');
+// function json(options){
+//     return (req,res,next)=>{
+//         console.log(`hello ${options.message}`);
+//         next()
+//     }
+// }
+//
+// app.use(json({
+//     message:'张三'
+// }));
 
-    // res.send({
-    //     foo:'bar'
-    // })
-    res.cookie('foo','bar');
-    res.cookie('a',123);
-    res.status(202).send('ok')
+// 中间件的顺序很重要
+// req 请求对象
+// res 响应对象
+// next 下一个中间件
+app.use((req,res,next)=>{
+    req.foo='bar';
+    res.abc=()=>{
+        console.log('abc')
+    }
+    console.log(req.method,req.url,Date.now())
+    // 交出执行权，往后继续匹配
+    next()
 })
 
+app.get('/',(req,res)=>{
+    console.log(req.foo)
+    res.send('get /')
+})
+app.get('/about',(req,res)=>{
+    res.abc()
+    res.send('get /about')
+})
+app.post('/login',(req,res)=>{
+    res.send('gpost /login')
+})
 app.listen(3000,()=>{
     console.log('Server running at http://localhost:3000/')
 })
+
+
+
+// const mylogger=(req)=>{
+//     console.log(req.method,req.url,Date.now())
+//
+// }
+//
+// app.get('/',(req,res)=>{
+//     mylogger(req);
+//     res.send('get /')
+// })
+// app.get('/about',(req,res)=>{
+//     mylogger(req);
+//     res.send('get /about')
+// })
+// app.post('/login',(req,res)=>{
+//     mylogger(req);
+//     res.send('gpost /login')
+// })
+// app.listen(3000,()=>{
+//     console.log('Server running at http://localhost:3000/')
+// })
