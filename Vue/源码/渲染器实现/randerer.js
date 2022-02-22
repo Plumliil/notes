eef300df06a072ab94d003233a36ba54f7c06e6b
plumli <476453574@qq.com>
2022-02-22 14:37:49 +0800

    vue中h函数,mount,patch的实现

--- Vue/源码/渲染器实现/randerer.js
+++ Vue/源码/渲染器实现/randerer.js
@@ -52,25 +52,25 @@ const patch = (n1, n2) => {
         const oldProps = n1.props || {};
         const newProps = n2.props || {};
         // 2.1获取所有newProps添加到el
-        for(const key in newProps){
-            const oldValue=oldProps[key];
-            const newValue=newProps[key];
-            if(newValue!==oldValue){
+        for (const key in newProps) {
+            const oldValue = oldProps[key];
+            const newValue = newProps[key];
+            if (newValue !== oldValue) {
                 if (key.startsWith('on')) {
                     // 事件监听的判断
                     el.addEventListener(key.slice(2).toLowerCase, newValue)
                 } else {
-                    el.setAttribute(key,newValue)
+                    el.setAttribute(key, newValue)
                 }
             }
         }
         // 2.2删除旧的props
-        for(const key in oldProps){
-            if(!(key in newProps)){
-                if(newValue!==oldValue){
+        for (const key in oldProps) {
+            if (!(key in newProps)) {
+                if (newValue !== oldValue) {
                     if (key.startsWith('on')) {
                         // 事件监听的判断
-                        const value=oldProps[key]
+                        const value = oldProps[key]
                         el.removeEventListener(key.slice(2).toLowerCase, value)
                     } else {
                         el.removeAttribute(key)
@@ -79,6 +79,45 @@ const patch = (n1, n2) => {
             }
         }
         // 处理children
-
+        const oldChildren = n1.children;
+        const newChildren = n2.children;
+        if (typeof newChildren === 'string') {
+            // 边界判断 (edge case)
+            if (typeof oldChildren === 'string') {
+                if (newChildren !== oldChildren) {
+                    el.textContent = newChildren
+                }
+            }
+            else {
+                el.innerHTML = newChildren;
+            }
+        } else {
+            // newChildren不是string而是数组
+            if (typeof oldChildren === 'string') {
+                el.innerHTML = '';
+                newChildren.forEach(item => {
+                    mount(item, el)
+                })
+            } else {
+                // 前面有相同节点的进行patch操作
+                const commonLength = Math.min(oldChildren.length, newChildren.length);
+                for (let i = 0; i < commonLength; i++) {
+                    patch(oldChildren[i], newChildren[i])
+                }
+                // newChildren.length > oldChildren.length
+                if (newChildren.length > oldChildren.length) {
+                    newChildren.slice(oldChildren.length).forEach(item => {
+                        mount(item, el)
+                    })
+                }
+                // newChildren.length < oldChildren.length
+                if(newChildren.length < oldChildren.length){
+                    oldChildren.slice(newChildren.length).forEach(item=>{
+                        el.removeChild(item.el)
+                    })
+                }
+                console.log(commonLength);
+            }
+        }
     }
 }
