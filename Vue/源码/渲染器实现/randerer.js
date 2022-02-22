736fe8d69c11ecb36ffa37abf7fb948ff17f5608
plumli <476453574@qq.com>
2022-02-22 11:18:03 +0800

    渲染器实现

--- Vue/源码/渲染器实现/randerer.js
+++ Vue/源码/渲染器实现/randerer.js
@@ -6,3 +6,79 @@ const h = (tag, props, children) => {
         children
     }
 }
+const mount = (vnode, container) => {
+    // 创建出真实元素，并在vnode上保留el
+    const el = vnode.el = document.createElement(vnode.tag);
+    console.log(el);
+    // 处理props
+    if (vnode.props) {
+        for (const key in vnode.props) {
+            const value = vnode.props[key]
+            if (key.startsWith('on')) {
+                // 事件监听的判断
+                el.addEventListener(key.slice(2).toLowerCase, value)
+            } else {
+                el.setAttribute(key, value)
+            }
+
+        }
+    }
+    // 处理children
+    console.log(vnode.children);
+    if (vnode.children) {
+        if (typeof vnode.children === 'string') {
+            el.textContent = vnode.children;
+        } else {
+            vnode.children.forEach(item => {
+                mount(item, el)
+            })
+        }
+    }
+    // 将el挂载到container中
+    container.appendChild(el)
+}
+
+const patch = (n1, n2) => {
+    console.log(n1, n2);
+    if (n1.tag !== n2.tag) {
+        const n1ElParent = n1.el.parentElement;
+        n1ElParent.removeChild(n1.el)
+        mount(n2, n1ElParent)
+    } else {
+        console.log(n1.el);
+        // 1.取出element对象,并n2中保存
+        const el = n2.el = n1.el;
+        // 2.处理props
+        const oldProps = n1.props || {};
+        const newProps = n2.props || {};
+        // 2.1获取所有newProps添加到el
+        for(const key in newProps){
+            const oldValue=oldProps[key];
+            const newValue=newProps[key];
+            if(newValue!==oldValue){
+                if (key.startsWith('on')) {
+                    // 事件监听的判断
+                    el.addEventListener(key.slice(2).toLowerCase, newValue)
+                } else {
+                    el.setAttribute(key,newValue)
+                }
+            }
+        }
+        // 2.2删除旧的props
+        for(const key in oldProps){
+            if(!(key in newProps)){
+                if(newValue!==oldValue){
+                    if (key.startsWith('on')) {
+                        // 事件监听的判断
+                        const value=oldProps[key]
+                        el.removeEventListener(key.slice(2).toLowerCase, value)
+                    } else {
+                        el.removeAttribute(key)
+                    }
+                }
+            }
+        }
+        // 处理children
+
+    }
+}
