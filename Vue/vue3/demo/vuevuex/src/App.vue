27be1413c3c70d4855de7883847763ac5442cdf4
plumli <476453574@qq.com>
2022-03-02 09:16:48 +0800

    vue3结束

--- Vue/vue3/demo/vuevuex/src/App.vue
+++ Vue/vue3/demo/vuevuex/src/App.vue
@@ -1,30 +1,48 @@
 <template>
   <div>
     <h1>App Vuex</h1>
-    <hr>
-    <h2>root:{{$store.state.rootCounter}}</h2>
+    <hr />
+    <h2>root:{{ $store.state.rootCounter }}</h2>
     <button @click="sub">-1</button>
     <button @click="add">+1</button>
-    <hr>
-    <home></home>
+    <hr />
+    <h2 ref="titleRef">{{ message }}</h2>
+    <button @click="addMsg">添加内容</button>
+    <!-- <home></home> -->
   </div>
 </template>
 
 <script>
-import Home from './pages/Home.vue'
+import { ref,nextTick } from "vue";
+// import Home from './pages/Home.vue'
 export default {
-  components:{
-    Home
+  components: {
+    // Home
   },
-  methods:{
-    sub(){
-      this.$store.commit('sub');
+  methods: {
+    sub() {
+      this.$store.commit("sub");
     },
-    add(){
-      this.$store.commit('add');
-    }
-  }
-}
+    add() {
+      this.$store.commit("add");
+    },
+  },
+  setup() {
+    const message = ref("哈哈哈哈");
+    const titleRef=ref(null);
+    const addMsg = () => {
+      message.value += "哈哈哈哈哈";
+      nextTick(()=>{
+        console.log(titleRef.value.offsetHeight);
+      })
+    };
+    return {
+      message,
+      addMsg,
+      titleRef
+    };
+  },
+};
 </script>
 
 <style>
