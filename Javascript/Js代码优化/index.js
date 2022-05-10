b0ffedc845cff49cb7949e79e303486e40b90db2
plumliil <476453574@qq.com>
2022-05-10 12:30:29 +0800

    js性能优化

--- Javascript/Js代码优化/index.js
+++ Javascript/Js代码优化/index.js
@@ -1,5 +1,11 @@
-let arrList = [];
+let arr = [1, 2, 3, 4, 5],
+    len = arr.length,
+    i = 1;
+for (; i < len; i++) {
+    console.log(arr[i]);
+}
 
-for (let i = arrList.length; i < 0; i--) {
-    console.log('');
+let arr1 = [1, 2, 3, 4, 5];
+for (let i = 1; i < arr1.length; i++) {
+    console.log(arr[i]);
 }
