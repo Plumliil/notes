1c909e6ff563a7c347c4e39a4782ef2ca52e28f3
plumliil <476453574@qq.com>
2022-11-07 17:38:23 +0800

    ts 继承

--- Typescript/dist/src/04_继承/index.js
+++ Typescript/dist/src/04_继承/index.js
@@ -1,10 +1,11 @@
 "use strict";
-function Parent() {
-    // name_: string, age_: number
-    //   console.log(name_ + age_ + "parent");
-    console.log("parent");
+function Parent(firstName) {
+    this.firstName = firstName;
 }
+Parent.prototype.intro = function () {
+    console.log("my firstname is zhang");
+};
 function Son(name_, age_) {
     console.log(name_ + age_ + "son");
 }
-Son.prototype = new Parent();
+Son.prototype = new Parent("zhang");
